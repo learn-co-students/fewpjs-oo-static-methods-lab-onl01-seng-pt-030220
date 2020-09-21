3093f20b1a9bb0c6e137177bbf1b7b5009d9ad9a
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-21 08:38:24 +0000

    Done.

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ class Formatter {
   }
 
   static  titleize(string){
-  let array=["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
-  //first char always capitalize
-  let first = string.replace(string[0],string[0].toUpperCase())
-  return   first.split(" ").map(el => {
-    if(! array.includes(el))
-     {  return el.replace(el[0],el[0].toUpperCase())
-     }else{ return el}
-     }
-
-   ).join(" ")
+    let array=["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    //first char always capitalize
+    let first = string.replace(string[0],string[0].toUpperCase())
+    return   first.split(" ").map(el => {
+      if(! array.includes(el))
+       {  return el.replace(el[0],el[0].toUpperCase())
+       }else{ return el}
+       }
+     ).join(" ")
   }
 
 }
