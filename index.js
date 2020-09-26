eb7c0b4809b7ed4f2b2f7edda2ca95f32b9cf8d2
alelee93 <leevargasale@gmail.com>
2020-09-25 20:31:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str){
+     // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return str.replace(/[^A-Za-z0-9- ']+/g,'')
+    //return str.replace(/[^A-Za-z0-9-']+/g, '')
+  }
+
+  static titleize(str){
+    let arr = str.split()
+    let newArr = arr.map(this.updateWord)
+    return newArr.join(" ")
+  }
+
+  updateWord(word){
+    let lowerCaseWord = word.toLowerCase()
+    switch(lowerCaseWord){
+      case "the":
+      case "a":
+      case "an":
+      case "but":
+      case "of":
+      case "and":
+      case "for":
+      case "at":
+      case "by":
+      case "from":
+        return word[0].toUpperCase() + word.slice(1)
+        break
+      default: 
+        return word.toUpperCase()
+    }
+  }
+
+
+
+    
 }
